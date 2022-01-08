886e72d0bf0d34d5e5750cf6876f7ce227536ff2
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-01-09 02:58:25 +0530

    Time: 92 ms (34.69%), Space: 51.3 MB (30.44%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,7 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
     unordered_map<int,int>mp;
     for(int i =0;i<nums.size();i++){
-        if(mp.find(nums[i])!=mp.end()) return true;
+        if(mp[nums[i]]==1) return true;
         else mp[nums[i]]=1;
     }
         return false;
