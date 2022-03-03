7c6a42a6b4b7607502ce34399faf72b12d8afd3c
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-03-03 16:03:10 +0530

    Time: 32 ms (96.03%), Space: 18.9 MB (84.77%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-    if(intervals.size()<=1) return intervals;
-    sort(intervals.begin(), intervals.end());
-    vector<vector<int>>result;
-    result.push_back(intervals[0]);
-    for(int i =1;i<intervals.size();i++){
-        if(result.back()[1]>=intervals[i][0]){
-            result.back()[1] = max(result.back()[1], intervals[i][1]);
-        }
-        else{
-            result.push_back(intervals[i]);
+    if(intervals.size()<=1)return intervals;
+    sort(intervals.begin(),intervals.end());
+    vector<vector<int>>results;
+    results.push_back(intervals[0]);
+    for(int i=1;i<intervals.size();i++){
+        if(intervals[i][0]<=results.back()[1]){
+            results.back()[1]= max(results.back()[1],intervals[i][1]);
+        }else{
+            results.push_back(intervals[i]);
         }
     }
-    return result;
+    return results;
     }
 };
