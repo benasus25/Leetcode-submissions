37828004de1770123d909fded1501c3ecc37325c
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-03-17 15:49:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
     int ans =0, count =0;
-    char prev = 'a';
-    for(auto i:s){
-        if(i=='(')count++;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='(')count++;
         else{
             count--;
-            if(prev=='(')ans +=pow(2,count);
+            if(s[i-1]=='(')ans +=pow(2,count);
         }
-        prev =i;
     }
     return ans;
     }
