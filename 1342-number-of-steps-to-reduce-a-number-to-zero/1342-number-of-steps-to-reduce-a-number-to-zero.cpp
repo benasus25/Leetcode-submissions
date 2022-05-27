97e0761a4ffd19c0f3e4660793ffc8d973d94598
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-05-27 14:41:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -3,14 +3,17 @@ public:
     int numberOfSteps(int num) {
     int count =0;
         while(num){
+            if(num==1){
+                count+=1;
+                break;
+            }
             if(num%2){
-                count++;
-                num-=1;
+                count+=2;;
             }
             else{
                 count++;
-                num/=2;
-            }
+            }   
+            num/=2;
         }
     return count;
     }
