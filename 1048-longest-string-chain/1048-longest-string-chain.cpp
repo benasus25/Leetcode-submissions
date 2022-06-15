f1b6473d208a273d62844fbea666e0416f9e106a
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-06-15 18:44:03 +0530

    Time: 161 ms (41.87%), Space: 31.3 MB (16.57%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        vector<unordered_set<string>> W(17);
-        for (auto word : words) 
+        vector<unordered_set<string>>W(17);
+        for(auto word:words){
             W[word.size()].insert(word);
-        unordered_map<string, int> dp;
-        int best = 1;
-        for (int i = 16; i; i--) {
-            if (W[i-1].empty()) continue;
-            for (auto word : W[i]) {
-                int wVal = dp[word] ? dp[word] : 1;
-                for (int j = 0; j < word.size(); j++) {
-                    string pred = word.substr(0,j) + word.substr(j+1);
-                    int pVal = dp[pred] ? dp[pred] : 1;
-                    if (W[i-1].find(pred) != W[i-1].end() && wVal >= pVal) {
-                        dp[pred] = wVal + 1;
-                        best = max(best, wVal + 1);
+        }
+        int res = 1;
+        unordered_map<string,int>dp;
+        for(int i=16;i;i--){
+            if(W[i-1].size()==0)continue;
+            for(auto word:W[i]){
+                int val = dp[word]?dp[word]:1;
+                for(int j = 0;j<word.size();j++){
+                    string temp = word.substr(0,j)+ word.substr(j+1);
+                    int val1 = dp[temp]?dp[temp]:1;
+                    if(W[i-1].find(temp)!=W[i-1].end()&& val>=val1){
+                        dp[temp]= val+1;
+                        res = max(res,val+1);
                     }
                 }
             }
         }
-        return best;
+        return res;
     }
 };
