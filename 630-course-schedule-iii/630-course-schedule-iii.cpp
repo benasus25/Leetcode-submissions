9f4269669ecccb26d84caf6c53e2f31e7d5d182b
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-06-23 21:25:02 +0530

    Time: 507 ms (23.44%), Space: 56.2 MB (85.57%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -6,10 +6,15 @@ public:
         int total = 0;
         for (auto &course : C) {
             int dur = course[0], end = course[1];
-            if (dur + total <= end) 
-                total += dur, pq.push(dur);
-            else if (pq.size() && pq.top() > dur)
-                total += dur - pq.top(), pq.pop(), pq.push(dur);
+            if (dur + total <= end){ 
+                total += dur;
+                pq.push(dur);
+            }
+            else if (pq.size() && pq.top() > dur){
+                total += dur - pq.top();
+                pq.pop();
+                pq.push(dur);
+            }
         }
         return pq.size();
     }
