19c00d0418b367ad130cdb6b25904745fb8b5dc5
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-07-02 18:27:01 +0530

    Time: 156 ms (90.05%), Space: 75.6 MB (62.96%) - LeetHub

--- 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
+++ 1953-maximum-number-of-weeks-for-which-you-can-work/1953-maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-    long long numberOfWeeks(vector<int>& milestones) {
-        long long unsigned int maxel = *max_element(milestones.begin(), milestones.end());
-        long long unsigned int sum = 0;
-        for (auto& m : milestones) sum += m;
-        if (sum - maxel >= maxel) return sum;
-        return (sum - maxel) * 2 + 1;
+    # define ll long long
+    long long numberOfWeeks(vector<int>& nums) {
+        ll total = 0;
+        total = accumulate(nums.begin(),nums.end(),total);
+        ll max = *max_element(nums.begin(),nums.end());
+        ll rest = total - max;
+        if(max>rest)
+            return 2*rest+1;
+        else return total;
     }
 };
