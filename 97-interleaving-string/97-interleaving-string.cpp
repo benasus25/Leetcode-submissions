dc9ba632fd6411be7e0b05cc8510201a879abb97
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-07-07 10:13:50 +0530

    Time: 4 ms (73.94%), Space: 7.2 MB (43.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    
-    bool helper(string s, string r, string t, int i, int j, int k, vector<vector<int>>&dp){
+    bool check(string &s1,string &s2,string &s3,int i,int j,int k,vector<vector<int>>&dp){
         if(dp[i][j]!=-1)return dp[i][j];
-        if(i==s.length()&&j==r.length()&&k==t.length())return true;
-        bool res = false;
-        if(i!=s.length()){
-            if(s[i]==t[k]){
-                res |= helper(s,r,t,i+1,j,k+1,dp);
-            }
-        }
-        if(j!=r.length()){
-            if(r[j]==t[k]){
-                res |= helper(s,r,t,i,j+1,k+1,dp);
-            }
-        }
-        return dp[i][j]= res;
+        if(i==s1.length() and j==s2.length() and k==s3.length())
+            return true;
+        bool res=false;
+        
+        if(i!=s1.length())
+            if(s1[i]==s3[k])
+            res|=check(s1,s2,s3,i+1,j,k+1,dp);
+        if(j!=s2.length())
+            if(s2[j]==s3[k])
+            res|=check(s1,s2,s3,i,j+1,k+1,dp);
+        return dp[i][j]=res;
     }
-    
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.length(),m = s2.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return helper(s1,s2,s3,0,0,0,dp);
+        int n=s1.length(),m=s2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        bool ans=check(s1,s2,s3,0,0,0,dp);
+        return ans;
     }
 };
