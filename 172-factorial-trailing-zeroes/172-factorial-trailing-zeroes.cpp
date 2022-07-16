ecd4acc2b52345de415a9f34afdfad84cd563669
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-07-16 15:05:40 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.58%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -3,8 +3,7 @@ public:
     int trailingZeroes(int n) {
         int count =0;
         while(n){
-            count +=  n/5;
-            n/=5;
+            count +=  n/=5;
         }
         return count;
     }
