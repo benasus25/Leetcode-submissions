ab4781d665a08ea523dbc0572a223a68ec85496e
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-07-18 12:08:48 +0530

    Time: 760 ms (90.59%), Space: 150.9 MB (68.11%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& M, int T) {
         int xlen = M[0].size(), ylen = M.size(), ans = 0;
-        unordered_map<int, int> res;
         for (int i = 0; i < ylen; i++)
             for (int j = 1; j < xlen; j++)
                 M[i][j] += M[i][j-1];
         for (int j = 0; j < xlen; j++)
             for (int k = j; k < xlen; k++) {
-                res.clear();
+                unordered_map<int, int> res;
                 res[0] = 1;
                 int csum = 0;
                 for (int i = 0; i < ylen; i++) {
