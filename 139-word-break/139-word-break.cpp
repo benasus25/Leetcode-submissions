ee29e9944fb06d7c7f41e3856ef6757e9e09728c
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-07-22 17:39:53 +0530

    Time: 18 ms (53.94%), Space: 13.3 MB (36.98%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    
-    bool recur(string s, unordered_map<string,int>&mp, int i,vector<int>&ispossible){
-        if(i>=s.length())return true;
-        if(ispossible[i]!=-1)return ispossible[i];
-        for(int j=i;j<s.length();j++){
-            if(mp.find(s.substr(i,j-i+1))!=mp.end()){
-                if(recur(s,mp,j+1,ispossible))return ispossible[i] = 1;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool>dp(s.size()+1,false);
+        unordered_set<string>mp(wordDict.begin(),wordDict.end());
+        dp[0]=true;
+        for(auto u:mp){
+            cout<<u<<endl;
+        }
+        for(int i=1;i<=s.length();i++){
+            for(int j =0;j<i;j++){
+                if(dp[j] && mp.find(s.substr(j,i-j))!=mp.end()){
+                    dp[i]=true;
+                    break;
+                }
             }
         }
-        return ispossible[i] = 0;
-    }
-    
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.length();
-        vector<int>ispossible(n,-1);
-        unordered_map<string,int>mp;
-        for(auto u:wordDict){
-            mp[u]++;
-        }
-        return recur(s,mp,0,ispossible);
+        return dp[s.size()];
     }
 };
