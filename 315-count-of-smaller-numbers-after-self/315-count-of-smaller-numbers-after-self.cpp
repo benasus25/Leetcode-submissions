dde21a06218665d9dcd94f2fbf84d171c71b3d00
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-07-23 16:20:37 +0530

    Time: 165 ms (98.96%), Space: 74.6 MB (92.12%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,21 +1,40 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-using namespace __gnu_pbds;
-
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>set_t;
+class BIT{
+private:
+    int n;
+    vector<int>tree;
+public:
+    BIT(int n){
+        this->n = n;
+        tree.resize(n+1,0);
+    }
+    void update(int x, int value){
+        for(;x<n;x+= x&(-x)){
+            tree[x] +=value;
+        }
+    }
+    int query(int x){
+        int sum = 0;
+        for(;x>0;x-=x&(-x)){
+            sum += tree[x];
+        }
+        return sum;
+    }
+};
 
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
+        int min_ele = abs(*min_element(nums.begin(),nums.end()));
+        for(int &u:nums){
+            u+=min_ele+1;
+        }
+        int maxi = abs(*max_element(nums.begin(),nums.end()));
+        BIT res (maxi+1);
         vector<int>ans(n,0);
-        set_t s;
-        s.insert(nums[n-1]);
-        for(int i=n-2;i>=0;i--){
-            int num  = s.order_of_key(nums[i]);
-            s.insert(nums[i]);
-            ans[i] = num;
+        for(int i=n-1;i>=0;i--){
+            ans[i] = res.query(nums[i]-1);
+            res.update(nums[i],1);
         }
         return ans;
     }
