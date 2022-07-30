0eee090398a95ba8add60ced169515ea89fabf4a
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-07-30 21:39:46 +0530

    Time: 331 ms (55.22%), Space: 51.5 MB (30.30%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int numMatchingSubseq(string& s, vector<string>& words) {
-        int ans = 0;
-        vector<vector<int>> mappings(26);  // stores array of indices for each character in s
-        for(int i = 0; i < size(s); i++) mappings[s[i] - 'a'].push_back(i);
-        for(auto& word : words) {
-            bool found = true;
-            // i = index in word | prev = index in s matched for word[i-1]
-            for(int i = 0, prev = -1; found && i < size(word); i++) {
-                auto& v = mappings[word[i]-'a'];
-                auto it = upper_bound(begin(v), end(v), prev);   // check if current character exists in s with index > prev
-                if(it == end(v)) found = false;                  // doesn't exist
-                else prev = *it;                                 // update prev for next check
+    int numMatchingSubseq(string s, vector<string>& words) {
+        vector<vector<int>>mappings(26);
+        for(int i=0;i<s.length();i++){
+            mappings[s[i]-'a'].push_back(i);
+        }
+        int ans  = 0;
+        for(auto &u:words){
+            bool flag = true;
+            int prev = -1;
+            for(int i=0;i<u.length() &&flag;i++){
+                auto &v = mappings[u[i]-'a'];
+                auto it = upper_bound(v.begin(),v.end(),prev);
+                if(it==v.end())flag = false;
+                else prev= *it;
             }
-            ans += found;
+            ans+=flag;
         }
         return ans;
     }
