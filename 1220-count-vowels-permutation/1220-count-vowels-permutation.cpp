3c0af0956649e6b69610749526b79f4c65dbe355
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-08-08 00:13:59 +0530

    Time: 3 ms (95.02%), Space: 6.6 MB (76.17%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,26 +1,18 @@
 class Solution {
-const int MOD = 1e9+7;
-const unordered_map<char, vector<char>>mp{{'s',{'a','e','i','o','u'}},
-                                          {'a',{'e'}},
-                                          {'e',{'a','i'}},
-                                          {'i',{'a','e','o','u'}},
-                                          {'o',{'i','u'}},
-                                          {'u',{'a'}}
-                                         };
-unordered_map<char,vector<int>>res;
 public:
-    
-    int solve(int n, char prev){
-        if(n==0)return 1;
-        if(res[prev][n])return res[prev][n];
-        for(auto c:mp.at(prev)){
-            res[prev][n] = (res[prev][n] + solve(n-1,c))%MOD;
-        }
-        return res[prev][n];
-    }
-    
     int countVowelPermutation(int n) {
-        res['s'] = res['a'] = res['e'] = res['i']= res['o'] = res['u'] = vector<int>(n+1);
-        return solve(n,'s');
+        const int MOD = 1e9+7;
+        long dp[5][n+1], ans = 0;
+        dp[0][1] = dp[1][1] = dp[2][1] = dp[3][1] = dp[4][1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[0][i] =  dp[1][i-1];
+            dp[1][i] = (dp[0][i-1] + dp[2][i-1]) % MOD;
+            dp[2][i] = (dp[0][i-1] + dp[1][i-1] + dp[3][i-1] + dp[4][i-1]) % MOD;
+            dp[3][i] = (dp[2][i-1] + dp[4][i-1]) % MOD;
+            dp[4][i] =  dp[0][i-1];
+        }
+        for(int i=0; i < 5; i++) 
+            ans = (ans + dp[i][n]) % MOD;
+        return ans;
     }
 };
