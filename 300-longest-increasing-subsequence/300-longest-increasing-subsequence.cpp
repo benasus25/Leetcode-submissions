715ea056346582e0aa277508a2d3aa7e7bc61005
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-08-08 09:49:03 +0530

    Time: 317 ms (64.17%), Space: 10.5 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>dp;
-        for(int i=0;i<nums.size();i++){
-            if(i==0)dp.push_back(nums[i]);
-            else{
-                auto x = lower_bound(dp.begin(),dp.end(), nums[i]) -dp.begin();
-                if(x>=dp.size()){
-                    dp.push_back(nums[i]);
-                }
-                else dp[x]=nums[i];
+        int n = nums.size(),ans = INT_MIN;
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j])dp[i]= max(dp[i],dp[j]+1);
             }
+            ans = max(ans,dp[i]);
         }
-        return dp.size();
+        return ans;
     }
 };
