201cdafdce098d1aee02f0308c56f363d579b778
Sparsh Sinha <sparshsinha.spavis@gmail.com>
2022-08-14 15:45:20 +0530

    Time: 226 ms (61.72%), Space: 13.8 MB (84.22%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>s(wordList.begin(),wordList.end());
-        queue<string>q;
-        if(s.find(endWord)==s.end())return 0;
-        q.push(beginWord);
-        int ans = 0;
-        while(!q.empty()){
-            ans++;
-            int len = q.size();
-            while(len--){
-                string curr = q.front();
-                q.pop();
-                for(int i=0;i<curr.size();i++){
-                    string temp = curr;
-                    for(char ch='a';ch<='z';ch++){
-                        temp[i]=ch;
-                        if(temp==curr)continue;
-                        if(temp==endWord)return ans+1;
-                        if(s.find(temp)!=s.end()){
-                            q.push(temp);
-                            s.erase(temp);
+    int ladderLength(string &beginWord, string &endWord, vector<string> &wordList){
+    	unordered_set<string> s(wordList.begin(), wordList.end());
+
+    	if(s.find(endWord) == s.end()) return 0;   // if our destination is not present in our list, return 0
+    	int cnt = 0;	      // to count the number of nodes/steps needed to reach the destination word
+    	queue<string> q;
+    	q.push(beginWord);	  // push the starting node into queue, as we did in BFS [beginWord is our root]
+    	
+    	while(!q.empty()){
+    		auto size = q.size();
+    		cnt++;
+    		while(size--){
+    			auto currWord = q.front(); q.pop();
+    			// find the next word which can be formed after currWord by changing it's letter(s)
+    			for(int i = 0; i < currWord.size(); ++i){
+    				string tmp = currWord;
+    				// try to change each character, make a new word find in set
+    				for(char ch = 'a'; ch <= 'z'; ++ch){
+    					tmp[i] = ch;			// makin' newWord
+    					// if new word is same as curr word simply ignore
+    					if(tmp.compare(currWord) == 0) continue;
+    					if(tmp.compare(endWord) == 0) return cnt + 1;	// found the endWord return cnt
+
+    					// if we find the newWord is present in our map. put in the queue 
+                        // as the next destination node, which will help is to reach our endWord
+                        if(s.find(tmp) != s.end()){
+                        	q.push(tmp);		// push the newWord or a node into queue 
+                        	s.erase(tmp);		// remove that word, cuz it's taken into the queue
                         }
-                    }
-                }
-            }
-        }
-        return 0;
+    				}
+    			}
+    		}
+    	}
+    	return 0;
     }
 };
